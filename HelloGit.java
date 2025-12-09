490edf4f2b7582e11339ea63ccbe3260ef05a7a1
Dong Wei <dongwei10237@gmail.com>
2025-12-09 16:34:30 +0800

    change class name; add new class notInStage

--- HelloGit.java
+++ HelloGit.java
@@ -1,4 +1,4 @@
-class demo {
+class HelloGit {
 
     public static void main(String[] args){
         System.out.println("Hello git");
---
+++ NotInStage.java
@@ -0,0 +1,5 @@
+public class NotInStage {
+    public static void main(String[] args) {
+        System.out.println("Current class have not been put into stage!");
+    }
+}
